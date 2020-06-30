63998ca4f9bdc95af526dbb40e508e73f3f908e5
Patrick W <sickday@pm.me>
2020-06-30 09:38:44 -0400

    ## Updated coverage for `shop.rb`, `test.rb`, `trade.rb`, `woodcutting.rb`
    Signed-off-by: Patrick W <Sickday@pm.me>
    ## Syntax updates to please the mighty rubocop.

--- plugins/test.rb
+++ plugins/test.rb
@@ -1,229 +1,254 @@
 
 # Logout
-on_int_button(2458) {|player|
-  player.io.send_logout
-}
+on_int_button(2458) { |player| player.io.send_logout }
 
-on_command("objspawn") {|player, params|
- temp_loc = player.location
-  
- object = RuneRb::Objects::Object.new(params[0].to_i, temp_loc, 2, params[1].to_i, -1, temp_loc, 0, params[2].to_i)
- object.change 
- 
- # Add this to the object manager
- WORLD.object_manager.objects << object
-}
+##
+# ::objspawn parameters command.
+# Spawns an object with the supplied parameters.
+on_command('objspawn') do |player, params|
+  temp_loc = player.location
+  object = RuneRb::Objects::Object.new(params[0].to_i, temp_loc, 2, params[1].to_i, -1, temp_loc, 0, params[2].to_i)
+  object.change
 
-on_command("pos") {|player, params|
-  player.io.send_message "You are at #{player.location.inspect}."
-}
-
-on_command("update") {|player, params|
+  # Add this to the object manager
+  WORLD.object_manager.objects << object
+end
+##
+# ::pos command.
+# Sends the context players position.
+on_command('pos') { |player, _| player.io.send_message("You are at #{player.location.inspect}.") }
+##
+# Schedules an update event with the WORLD.
+# ::update time
+on_command('update') do |_player, params|
   time = params.first.to_i
-  WORLD.submit_event RuneRb::Tasks::SystemUpdateEvent.new(time)
-}
-
-on_command("dc") {|player, params|
-  player.connection.close_connection
-}
-
-on_command("goup") {|player, params|
-  player.teleport_location = player.location.transform(0, 0, 1)  
-}
-
-on_command("godown") {|player, params|
-  player.teleport_location = player.location.transform(0, 0, -1)  
-}
-
-on_command("item") {|player, params|
+  WORLD.submit_event(RuneRb::Tasks::SystemUpdateEvent.new(time))
+end
+##
+# ::dc playername
+# Forcibly disconnects a player.
+on_command('dc') { |player, _params| player.connection.close_connection }
+##
+# ::goup
+# Teleports the player to 1 height level above their current position.
+on_command('goup') { |player, _params| player.teleport_location = player.location.transform(0, 0, 1) }
+##
+# ::godown
+# Teleports the player to 1 height level below their current position.
+on_command('godown') { |player, _params| player.teleport_location = player.location.transform(0, 0, -1) }
+##
+# ::item params
+# Spawns an item given it's id and count.
+on_command('item') do |player, params|
   id = params[0].to_i
   count = params.length == 2 ? params[1].to_i : 1
   player.inventory.add RuneRb::Item::Item.new(id, count)
-}
-
-on_command("design") {|player, params|
-  player.io.send_interface 3559
-}
-
-on_item_on_player(1050) {|player, used_player|
-   player.io.send_message "You used 1050 on a player"
-   used_player.io.send_message "someone used you" 
-}
-
-on_item_on_npc(1050, 1) {|player, npc|
-   player.io.send_message "You used a santa on an npc" 
-}
-
-on_int_button(3651) {|player|
-  player.io.send_clear_screen
-}
-
-on_command("reload") {|player, params|
-  player.io.send_message "Reloading..." 
+end
+##
+# ::design
+# Opens the makeover interface
+on_command('design') { |player, _params| player.io.send_interface(3559) }
+##
+# ::reload
+# Reloads the server
+on_command('reload') do |player, _params|
+  player.io.send_message 'Reloading...'
   SERVER.reload
-}
+end
+##
+# ::spawn parameters
+# Spawns an npc given it's ID.
+on_command('spawn') do |player, params|
+  id = params[0].to_i
+  npc = RuneRb::NPC::NPC.new RuneRb::NPC::NPCDefinition.for_id(id)
+  npc.location = player.location.transform(1, 1, 0)
 
-on_command("spawn") {|player, params|
-   id = params[0].to_i
-   npc = RuneRb::NPC::NPC.new RuneRb::NPC::NPCDefinition.for_id(id)
-   npc.location = player.location.transform(1, 1, 0)
-   
-   WORLD.register_npc npc
-}
-
-on_command("cfg") {|player, params|
+  WORLD.register_npc(npc)
+end
+##
+# ::cfg parameters
+# Sets the configuration given it's ID and the value to set it to.
+on_command('cfg') do |player, params|
   id = params[0].to_i
   value = params[1].to_i
-  player.io.send_config id, value
-  player.io.send_message "Setting #{id} to #{value}"
-}
-
-on_npc_option2(592) {|player, npc|
-  player.io.send_message "Open shop"
-}
-
-on_command("teleto") {|player, params|
+  player.io.send_config(id, value)
+  player.io.send_message("Setting #{id} to #{value}")
+end
+##
+# ::teleto parameters
+# Attempts to teleport to a player given their name.
+on_command('teleto') do |player, params|
   target = get_player(params[0])
-  unless target == nil
-    player.teleport_location = target.location
-    player.io.send_message "You were teleported to #{target.name}."
-    target.io.send_message "#{player.name} teleported to you."
+  if target.nil?
+    player.io.send_message('User not found.')
   else
-    player.io.send_message "User not found."
+    player.teleport_location = target.location
+    player.io.send_message("You were teleported to #{target.name}.")
+    target.io.send_message("#{player.name} teleported to you.")
   end
-}
-
-# Have the player enter a number, and see if
-# it is correct.
-on_command("guess") {|player, params|
+end
+##
+# ::guess parameters
+# A simple guessing game to test rng? The player will get a message if they successfully guess the same number we generate.
+on_command('guess') do |player, params|
   # Turn the string into an integer using to_i
   number = params[0].to_i
 
-  if number == 1234
-    player.io.send_message "Yay! You win!"
+  if number == rand(256)
+    player.io.send_message('Yay! You win!')
   else
-    player.io.send_message "You guessed wrong!"
+    player.io.send_message('You guessed wrong!')
   end
-}
-
-on_command("tele") {|player, params|
+end
+##
+# ::tele parameters
+# Teleports the player to the supplied coordinates
+on_command('tele') do |player, params|
   x = params[0].to_i
   y = params[1].to_i
   z = params.length > 2 ? params[2].to_i : 0
   loc = RuneRb::Model::Location.new(x, y, z)
   player.io.send_message "Teleporting to #{loc.inspect}..."
   player.teleport_location = loc
-}
-
-on_command("snow") {|player, params|
-  player.io.send_interface(11877, true)
-}
-
-on_command("teletome") {|player, params|
+end
+##
+# ::snow
+# Starts the snow effect.
+on_command('snow') { |player, _params| player.io.send_interface(11_877, true) }
+##
+# ::teletome
+# Attempts to teleport the specified player to the context player.
+on_command('teletome') do |player, params|
   target = get_player(params[0])
-  unless target == nil
-    target.teleport_location = player.location
-    player.io.send_message "#{target.name} was teleported to you."
-    target.io.send_message "You were teleported to #{player.name}."
+  if target.nil?
+    player.io.send_message('User not found.')
   else
-    player.io.send_message "User not found."
+    target.teleport_location = player.location
+    player.io.send_message("#{target.name} was teleported to you.")
+    target.io.send_message("You were teleported to #{player.name}.")
   end
-}
-
-on_command("teleall") {|player, params|
-  WORLD.players.each {|target|
-    if target != nil and target.name != player.name
+end
+##
+# ::teleall
+# Attempts to teleport all players in the world to the context player.
+on_command('teleall') do |player, _params|
+  WORLD.players.each do |target|
+    if !target.nil? && (target.name != player.name)
       target.teleport_location = player.location
-      target.io.send_message "You were teleported to #{player.name}."
+      target.io.send_message("You were teleported to #{player.name}.")
     end
-  }
-}
-
-on_command("move") {|player, params|
-  player.flags.flag :forced_move
-}
-
-on_command("em") {|player, params|
+  end
+end
+##
+# ::move
+# Toggles the forced_move flag for the context player.
+on_command('move') { |player, _params| player.flags.flag(:forced_move) }
+##
+# ::em parameter
+# Returns the value of a player object's variable with the same name as the first parameter.
+# TODO: Unsafe use of Kernel#eval should be removed.
+on_command('em') do |player, params|
   val = eval("player.#{params.first}")
   player.io.send_message "returned: #{val.inspect}"
-}
-
-on_command("g") {|player, params|
+end
+##
+# ::g
+# Forces the player to face East.
+on_command('g') do |player, _params|
   x = player.location.x + 1
   y = player.location.y
   z = player.location.z
 
-  player.face RuneRb::Model::Location.new(x, y, z)
-}
-
-on_command("max") {|player, params|
-  RuneRb::Player::Skills::SKILLS.each { |skill|
-    player.skills.set_skill skill, 99, 13034431
-  }
-  player.flags.flag :appearance
-}
-
-on_command("md") {|player, params|
+  player.face(RuneRb::Model::Location.new(x, y, z))
+end
+##
+# ::max
+# Maxes out the player's stats.
+on_command('max') do |player, _params|
+  RuneRb::Player::Skills::SKILLS.each do |skill|
+    player.skills.set_skill(skill, 99, 13_034_431)
+  end
+  player.flags.flag(:appearance)
+end
+##
+# ::md parameter
+# Sets the context player's model to the supplied id
+on_command('md') do |player, params|
   player.model = params.first.to_i
   player.flags.flag :appearance
-}
-
-on_command("sa") {|player, params|
+end
+##
+# ::sa parameter
+# Sets the context player's standing animation to the supplied id.
+on_command('sa') do |player, params|
   player.standanim = params.first.to_i
   player.flags.flag :appearance
-}
-
-on_command("wa") {|player, params|
+end
+##
+# ::wa parameter
+# Sets the context player's walking animation to the supplied id.
+on_command('wa') do |player, params|
   player.walkanim = params.first.to_i
   player.flags.flag :appearance
-}
-
-on_command("empty") {|player, params|
+end
+##
+# ::empty
+# Clears the context player's inventory.
+on_command('empty') do |player, _params|
   player.inventory.clear
   player.inventory.fire_items_changed
-}
-
-on_item_on_obj(1050, 1278) {|player, loc|
-  player.io.send_message "Holy shit batman."
-}
-
+end
+##
+# Attmpts to retreived a player given their name. 
+# TODO: rename this function and perhaps move it to the WORLD object?
+# `WORLD#by_name(name) seems cleaner`
 def self.get_player(name)
-  WORLD.players.find {|e| e.name.downcase == name.downcase }
+  WORLD.players.find { |e| e.name.downcase == name.downcase }
 end
 
-@@emotes = {
-  161 => 860,
-  162 => 857,
-  163 => 863,
-  164 => 858,
-  165 => 859,
-  166 => 866,
-  167 => 864,
-  168 => 855,
-  169 => 856,
-  170 => 861,
-  171 => 862,
-  172 => 865,
-  13362 => 2105,
-  13363 => 2106,
-  13364 => 2107,
-  13365 => 2108,
-  13366 => 2109,
-  13367 => 2110,
-  13368 => 2111,
-  13383 => 2127,
-  13384 => 2128,
-  13369 => 2112,
-  13370 => 2113,
-  11100 => 1368,
-  667 => 1131,
-  6503 => 1130,
-  6506 => 1129,
-  666 => 1128,
-}
+{161 => 860,
+ 162 => 857,
+ 163 => 863,
+ 164 => 858,
+ 165 => 859,
+ 166 => 866,
+ 167 => 864,
+ 168 => 855,
+ 169 => 856,
+ 170 => 861,
+ 171 => 862,
+ 172 => 865,
+ 13_362 => 2105,
+ 13_363 => 2106,
+ 13_364 => 2107,
+ 13_365 => 2108,
+ 13_366 => 2109,
+ 13_367 => 2110,
+ 13_368 => 2111,
+ 13_383 => 2127,
+ 13_384 => 2128,
+ 13_369 => 2112,
+ 13_370 => 2113,
+ 11_100 => 1368,
+ 667 => 1131,
+ 6503 => 1130,
+ 6506 => 1129,
+ 666 => 1128}.each do |button, anim|
+  on_int_button(button) { |player| player.play_animation(RuneRb::Model::Animation.new(anim)) }
+end
+=begin
+on_item_on_player(1050) do |player, used_player|
+  player.io.send_message('You used 1050 on a player')
+  used_player.io.send_message('someone used 1050 on you')
+end
 
-@@emotes.each {|button, anim|
-  on_int_button(button) {|player|
-    player.play_animation RuneRb::Model::Animation.new(anim)
-  }
-}
+on_item_on_npc(1050, 1) do |player, _npc|
+  player.io.send_message 'You used a santa on an npc'
+end
+
+on_int_button(3651) do |player|
+  player.io.send_clear_screen
+end
+on_npc_option2(592) do |player, _npc|
+  player.io.send_message 'Open shop'
+end=end
